cb1b734cb3a05b91a6f4b08d460359c2f3050c39
Michael Vega <ichigo509@yahoo.com>
2019-10-28 13:16:03 -0400

    Updates

---
+++ precache-manifest.623e4a9f906127f2860784b5faa4c102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4423630ed70f7d55b8373c97df7dccb8",
+    "url": "/Clicky-Game-React/index.html"
+  },
+  {
+    "revision": "bb99c03da561bfef1928",
+    "url": "/Clicky-Game-React/static/css/main.f750e8f8.chunk.css"
+  },
+  {
+    "revision": "40ac66acb306f5f2e931",
+    "url": "/Clicky-Game-React/static/js/2.495ebc39.chunk.js"
+  },
+  {
+    "revision": "bb99c03da561bfef1928",
+    "url": "/Clicky-Game-React/static/js/main.524a19bd.chunk.js"
+  },
+  {
+    "revision": "f216b5e0e0f49593441a",
+    "url": "/Clicky-Game-React/static/js/runtime-main.626ced92.js"
+  }
+]);
